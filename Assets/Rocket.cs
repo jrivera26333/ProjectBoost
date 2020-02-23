3dedeb461fe59c9f351b924234e1f4a67f9372d7
jrivera26333 <jrivera26333@gmail.com>
2020-02-23 00:46:11 -0600

    Added a rotation to the thrust

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -21,16 +23,16 @@ public class Rocket : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.Space)) //Seperate if so we can thrust and rotate
         {
-            print("Space Pressed");
+            rigidbody.AddRelativeForce(Vector3.up);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            print("Rotating Left");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.P))
         {
-            print("Rotating Right");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
